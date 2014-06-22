5d07724cd00bb16db4c8b36780d08e8509ee180e
Candy Hoang <candyhoang@gmail.com>
2014-06-22 12:21:23 +0100

    Use koa-session and koa-views in default app.

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 var packageJson = require(__dirname + '/package.json');
 
+var open = require('open');
 var koa = require('koa');
 var logger = require('koa-logger');
 var mount = require('koa-mount');
 var router = require('koa-router');
+var session = require('koa-session');
+var views = require('koa-views');
 
 var api = require(__dirname + '/applications/api');
 var database = require(__dirname + '/applications/database');
@@ -23,10 +26,10 @@ app.use(function *(next) {
 // use koa-router
 app.use(router(app));
 
-app.get('/', function *(next) {
-  yield next;
-  this.body = 'Welcome to the main application';
-});
+// app.get('/', function *(next) {
+//   yield next;
+//   this.body = 'Welcome to the main application';
+// });
 
 demo.use(function *(next){
   yield next;
@@ -39,10 +42,36 @@ app.use(mount('/api', api));
 app.use(mount('/database', database));
 app.use(mount('/socket', socket));
 
+// setup session
+app.keys = ['secrets'];
+app.use(session());
+
+// serve views
+app.use(views({
+  cache: true,
+
+  map: {
+    html: 'underscore'
+  }
+}));
+
+app.use(function* (next) {
+  var n = this.session.views || 0;
+  this.session.views = ++n;
+
+  this.locals = {
+    session: this.session
+  };
+
+  yield this.render('source/views/index', {
+    user: 'John'
+  });
+});
+
 // set koa to listen on specified port
 if (!module.parent) {
   app.listen(packageJson.config.server.koa.port);
 }
 
-// open('http://localhost:' + packageJson.config.server.koa.port);
+open('http://localhost:' + packageJson.config.server.koa.port);
 console.info('main Koa application now running on http://localhost:' + packageJson.config.server.koa.port);
