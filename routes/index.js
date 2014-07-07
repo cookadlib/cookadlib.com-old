7a6157ea22fd3e5d7d8133e1d4b9fd3388d28386
Karl Podger <karl.podger@primeordinal.com>
2014-07-07 02:27:57 +0100

    Add sample Koa code.

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
-
-/** serve jade enabled partials */
-exports.partials = function(req, res) {
-  res.render('partials/' + req.params.name);
-};
--- source/scripts/source/router.js
+++ source/scripts/source/router.js
@@ -1,44 +1,42 @@
-/* global angular */
-var cookAdLibApp = angular.module('cookAdLibApp', ['ui.router']);
-
-cookAdLibApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  'use strict';
-  //
-  // For any unmatched url, redirect to /state1
-  $urlRouterProvider.otherwise('/');
-  //
-  // Now set up the states
-  $stateProvider
-  .state('start', {
-    url: '/',
-    templateUrl: 'elements/home.html'
-  })
-  .state('recipes', {
-    url: '/recipes',
-    templateUrl: 'elements/recipes.html'
-  })
-  .state('state1', {
-    url: '/state1',
-    templateUrl: 'elements/state1.html'
-  })
-  .state('state1.list', {
-    url: '/list',
-    templateUrl: 'elements/state1.list.html',
-    controller: function($scope) {
-      $scope.items = ['A', 'List', 'Of', 'Items'];
-    }
-  })
-  .state('state2', {
-    url: '/state2',
-    templateUrl: 'elements/state2.html'
-  })
-  .state('state2.list', {
-    url: '/list',
-    templateUrl: 'elements/state2.list.html',
-    controller: function($scope) {
-      $scope.things = ['A', 'Set', 'Of', 'Things'];
-    }
-  });
-
-  $locationProvider.html5Mode(true);
-});
+// var cookAdLibApp = angular.module('cookAdLibApp', ['ui.router']);
+// cookAdLibApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+//   'use strict';
+//   //
+//   // For any unmatched url, redirect to /state1
+//   $urlRouterProvider.otherwise('/');
+//   //
+//   // Now set up the states
+//   $stateProvider
+//   .state('start', {
+//     url: '/',
+//     templateUrl: 'elements/home.html'
+//   })
+//   .state('recipes', {
+//     url: '/recipes',
+//     templateUrl: 'elements/recipes.html'
+//   })
+//   .state('state1', {
+//     url: '/state1',
+//     templateUrl: 'elements/state1.html'
+//   })
+//   .state('state1.list', {
+//     url: '/list',
+//     templateUrl: 'elements/state1.list.html',
+//     controller: function($scope) {
+//       $scope.items = ['A', 'List', 'Of', 'Items'];
+//     }
+//   })
+//   .state('state2', {
+//     url: '/state2',
+//     templateUrl: 'elements/state2.html'
+//   })
+//   .state('state2.list', {
+//     url: '/list',
+//     templateUrl: 'elements/state2.list.html',
+//     controller: function($scope) {
+//       $scope.things = ['A', 'Set', 'Of', 'Things'];
+//     }
+//   });
+//
+//   $locationProvider.html5Mode(true);
+// });
