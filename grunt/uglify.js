41c08138f7d9ce7c434ca2ad66abe267e83501a6
Karl Podger <karl.podger@primeordinal.com>
2014-07-10 22:50:52 +0100

    Include Modernizr, Lodash, DreamFactory and change Angular routers used.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -18,7 +18,8 @@ module.exports = {
 
     files: {
       '<%= package.config.path.source %>/scripts/main.js': [
-        '<%= package.config.path.source %>/scripts/source/**/*.js'
+        '<%= package.config.path.source %>/scripts/source/**/*.js',
+        '!<%= package.config.path.source %>/scripts/source/_sample/**/*.js'
       ]
     }
 
@@ -28,6 +29,7 @@ module.exports = {
 
     options: {
       // banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
+      compress: false,
       mangle: false,
       sourceMappingURL: '<%= package.config.path.source %>/scripts/vendor.map'
     },
@@ -35,9 +37,13 @@ module.exports = {
     files: {
       '<%= package.config.path.source %>/scripts/vendor.js': [
         // '<%= package.config.path.source %>/bower_components/jquery/dist/jquery.js',
+        '<%= package.config.path.source %>/bower_components/modernizr/modernizr.js',
+        '<%= package.config.path.source %>/bower_components/lodash/dist/lodash.compat.js',
         '<%= package.config.path.source %>/bower_components/angular/angular.js',
-        '<%= package.config.path.source %>/bower_components/angular-ui-router/release/angular-ui-router.js',
+        '<%= package.config.path.source %>/bower_components/angular-route/angular-route.js',
+        // '<%= package.config.path.source %>/bower_components/angular-ui-router/release/angular-ui-router.js',
         '<%= package.config.path.source %>/bower_components/restangular/dist/restangular.js',
+        // '<%= package.config.path.source %>/bower_components/angular-dreamfactory/angular-dreamfactory.js',
         '<%= package.config.path.source %>/bower_components/platform/platform.js',
         '<%= package.config.path.source %>/bower_components/socket.io-client/socket.io.js'
       ]
