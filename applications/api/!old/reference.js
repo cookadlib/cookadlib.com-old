daa1ee67ce0db0d6d15011919d939ef2b10088f0
Karl Podger <karl.podger@primeordinal.com>
2014-07-15 00:11:41 +0100

    Add reference code for DBPedia.

--- applications/api/!old/reference.js
+++ applications/api/!old/reference.js
@@ -1,3 +1,13 @@
+// https://developers.google.com/freebase/v1/search-metaschema
+// https://www.npmjs.org/package/koa-request
+// http://wiki.dbpedia.org/UseCases
+// http://wiki.dbpedia.org/Downloads39
+// http://lod.geospecies.org/
+// http://www.geonames.org/
+// http://www.gutenberg.org/
+// http://linkedgeodata.org/About
+// http://dbpedia.org/sparql
+
 var database = require(__dirname + '/database');
 
 var db = new database('localhost/starwars');
@@ -56,3 +66,37 @@ app.get('/book/:id', api.book.read);
 //
 //   this.body = res;
 // });
+
+
+// Routes: Test
+
+api.get('/test', function *(next) {
+  rdfstore.create(function(store) {
+    store.execute('LOAD <http://dbpedia.org/resource/Tim_Berners-Lee> INTO GRAPH <http://example.org/people>', function() {
+      store.setPrefix('dbp', 'http://dbpedia.org/resource/');
+      store.node(store.rdf.resolve('dbp:Tim_Berners-Lee'), "http://example.org/people", function(success, graph) {
+        var peopleGraph = graph.filter(store.rdf.filters.type(store.rdf.resolve("foaf:Person")));
+
+        store.execute(
+          'PREFIX rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\
+           PREFIX foaf: <http://xmlns.com/foaf/0.1/>\
+           PREFIX : <http://example.org/>\
+           SELECT ?s FROM NAMED :people { GRAPH ?g { ?s rdf:type foaf:Person } }',
+          function(success, results) {
+            var result = peopleGraph.toArray()[0].subject.valueOf() === results[0].s.value;
+            console.log('rdf', result);
+            var res = result;
+            this.body = res;
+          });
+      });
+    });
+  });
+});
+
+
+// var mid = result['result'][0].mid;
+
+// var topic = yield freebaseTopic (mid);
+// console.log('topic', topic);
+
+// this.body = topic;
