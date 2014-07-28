9090591615ec09fcb29d88a3a4586387ec8d2685
Karl Podger <karl.podger@primeordinal.com>
2014-07-28 13:11:12 +0100

    Add contextual homepage styling depending on whether the background is white or an image.

--- source/scripts/source/navigate-full-viewport-sections.js
+++ source/scripts/source/navigate-full-viewport-sections.js
@@ -5,7 +5,10 @@ $(document).ready(function () {
 
   functions.Navigation = window.functions = window.functions || {};
 
-  functions.Navigation.$sections = $('main > section');
+  functions.Navigation.sections = 'main > section';
+  functions.Navigation.$sections = $(functions.Navigation.sections);
+  functions.Navigation.pills = 'main > .pills';
+  functions.Navigation.$pills = $(functions.Navigation.pills);
   functions.Navigation.sectionLength = functions.Navigation.$sections.length - 1; // compensate for comparing a count to a 0-based index
   functions.Navigation.currentSection = 0;
   functions.Navigation.targetSection = 0;
@@ -18,6 +21,15 @@ $(document).ready(function () {
         functions.Navigation.currentSection = $(this).index();
       }
     });
+
+    functions.Navigation.$sections.eq(functions.Navigation.currentSection).addClass('active').siblings().removeClass('active');
+    $('a', functions.Navigation.$pills).eq(functions.Navigation.currentSection).addClass('active').siblings().removeClass('active');
+
+    if(functions.Navigation.$sections.eq(functions.Navigation.currentSection).hasClass('background')) {
+      $('body').addClass('inverse-lightness');
+    } else {
+      $('body').removeClass('inverse-lightness');
+    }
   };
 
   functions.Navigation.KeyboardSnap = function () {
@@ -83,14 +95,16 @@ $(document).ready(function () {
 
   functions.Navigation.ScrollSnap = function () {
     $(window).on('scroll touchmove', function() {
-      console.log('scroll touchmove');
+      // console.log('scroll touchmove');
       if (functions.Navigation.scrollTimeout) {
-        console.log('clearTimeout');
+        // console.log('clearTimeout');
         clearTimeout(functions.Navigation.scrollTimeout);
       }
-      console.log('between');
+      // console.log('between');
+
       functions.Navigation.scrollTimeout = setTimeout(function() {
-        console.log('stopped');
+        // console.log('stopped');
+        $('html, body').stop();
 
         var difference = 0;
 
@@ -110,11 +124,32 @@ $(document).ready(function () {
 
         functions.Navigation.SetCurrentSection();
 
-      }, 1000);
+      }, 250);
+    });
+  };
+
+  functions.Navigation.Pills = function () {
+    functions.Navigation.$sections.each(function() {
+      functions.Navigation.$pills.append('<a href="#' + $(this).attr('id') + '"><span>Jump to #' + $(this).attr('id') + '</span></a>');
+    });
+
+    $('body').on('click', functions.Navigation.pills + ' a', function(event) {
+      event.preventDefault();
+
+      $(this).siblings().removeClass('active');
+      $(this).addClass('active');
+
+      functions.Navigation.$targetSection = $($(this).attr('href'));
+
+      $('html, body').animate({
+        scrollTop: functions.Navigation.$targetSection.offset().top
+      }, 250, functions.Navigation.SetCurrentSection);
+
     });
   };
 
   functions.Navigation.KeyboardSnap();
   functions.Navigation.ScrollSnap();
+  functions.Navigation.Pills();
 
 });
